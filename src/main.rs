eb2a1987ca7fec4ea5cbaa97b700e4b6ac71c54d
63square <51100444+63square@users.noreply.github.com>
2025-09-07 18:38:58 +0100

    feat: swap to parquet format

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use arrow::array::{Array, StringArray};
 use indexmap::IndexSet;
 use ipnet::IpNet;
 use ipnet_trie::IpnetTrie;
+use parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
 use std::env;
+use std::fs::File;
 use std::net::{IpAddr, ToSocketAddrs};
+use std::path::Path;
 use std::pin::Pin;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -19,19 +23,20 @@ mod pb {
     tonic::include_proto!("grpc.ipasn.lookup");
 }
 
-type Record = (
-    String,
-    String,
-    String,
-    String,
-    String,
-    String,
-    String,
-    String,
-);
+struct U24(u8, u8, u8);
 
-type IpInfo = u32;
-type Table = IpnetTrie<IpInfo>;
+impl U24 {
+    pub fn new(x: u32) -> U24 {
+        assert!(x <= 0xFFFFFF, "out of bounds");
+        U24((x >> 16) as u8, ((x >> 8) & 0xFF) as u8, (x & 0xFF) as u8)
+    }
+
+    pub fn read(&self) -> u32 {
+        return ((self.0 as u32) << 16) | ((self.1 as u32) << 8) | self.2 as u32;
+    }
+}
+
+type Table = IpnetTrie<U24>;
 
 #[derive(Eq, PartialEq)]
 struct ContinentInfo {
@@ -75,55 +80,127 @@ struct IPData {
     continents: IndexSet<ContinentInfo>,
     countries: IndexSet<CountryInfo>,
     asns: IndexSet<ASInfo>,
+    ip_data: IndexSet<u32>,
 }
 
 const CONTINENT_BITS: u32 = 4;
 const COUNTRY_BITS: u32 = 8;
 const ASN_SHIFT: u32 = CONTINENT_BITS + COUNTRY_BITS;
 
-fn pack_ip_info(asn: u32, country: u8, continent: u8) -> IpInfo {
+fn pack_ip_info(asn: u32, country: u8, continent: u8) -> u32 {
     (asn << (COUNTRY_BITS + CONTINENT_BITS))
         | ((country as u32) << CONTINENT_BITS)
         | (continent as u32)
 }
 
-fn unpack_ip_info(ip_info: IpInfo) -> (u32, u8, u8) {
-    let continent = (ip_info & 0xF) as u8;
-    let country = ((ip_info >> CONTINENT_BITS) & 0xFF) as u8;
-    let asn = ip_info >> ASN_SHIFT;
+fn unpack_ip_info(code: u32) -> (u32, u8, u8) {
+    let continent = (code & 0xF) as u8;
+    let country = ((code >> CONTINENT_BITS) & 0xFF) as u8;
+    let asn = code >> ASN_SHIFT;
+
     (asn, country, continent)
 }
 
 fn load_data(data_path: &str) -> IPData {
-    let mut table: IpnetTrie<IpInfo> = IpnetTrie::new();
+    let mut table: Table = IpnetTrie::new();
     let mut continents: IndexSet<ContinentInfo> = IndexSet::with_capacity(8);
     let mut countries: IndexSet<CountryInfo> = IndexSet::with_capacity(255);
     let mut asns: IndexSet<ASInfo> = IndexSet::with_capacity(150000);
+    let mut ip_data: IndexSet<u32> = IndexSet::new();
 
-    let mut data_reader = csv::Reader::from_path(data_path).expect("unable to read data");
+    let file = File::open(Path::new(data_path)).expect("unable to read file");
 
-    for result in data_reader.deserialize() {
-        let mut record: Record = result.expect("unable to read record");
+    let mut arrow_reader = ParquetRecordBatchReaderBuilder::try_new(file)
+        .expect("unable to read parquet")
+        .build()
+        .unwrap();
 
-        if let Ok(network) = IpNet::from_str(&record.0) {
+    while let Some(batch) = arrow_reader.next() {
+        let batch = batch.unwrap();
+
+        let network_array = batch
+            .column(0)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let country_array = batch
+            .column(1)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let country_code_array = batch
+            .column(2)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let continent_array = batch
+            .column(3)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let continent_code_array = batch
+            .column(4)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let asn_array = batch
+            .column(5)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let as_name_array = batch
+            .column(6)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+        let as_domain_array = batch
+            .column(7)
+            .as_any()
+            .downcast_ref::<StringArray>()
+            .unwrap();
+
+        for i in 0..batch.num_rows() {
+            let record_network = network_array.value(i);
+
+            let record_country = country_array.value(i);
+            let record_country_code = country_code_array.value(i);
+
+            let record_continent = continent_array.value(i);
+            let record_continent_code = continent_code_array.value(i);
+
             let (continent, _) = continents.insert_full(ContinentInfo {
-                continent: record.3,
-                continent_code: record.4.into_bytes().try_into().unwrap(),
-            });
-            let (country, _) = countries.insert_full(CountryInfo {
-                country: record.1,
-                country_code: record.2.into_bytes().try_into().unwrap(),
+                continent: record_continent.to_owned(),
+                continent_code: record_continent_code.as_bytes().try_into().unwrap(),
             });
 
-            let (asn, _) = asns.insert_full(ASInfo {
-                asn: if record.5.len() > 2 {
-                    record.5[2..].parse().unwrap_or(0)
-                } else {
-                    0
-                },
-                as_name: record.6,
-                as_domain: record.7,
+            let (country, _) = countries.insert_full(CountryInfo {
+                country: record_country.to_owned(),
+                country_code: record_country_code.as_bytes().try_into().unwrap(),
             });
+
+            let as_info = if !asn_array.is_null(i) {
+                let record_asn = asn_array.value(i);
+                let record_as_name = as_name_array.value(i);
+                let record_as_domain = as_domain_array.value(i);
+
+                ASInfo {
+                    asn: if record_asn.len() > 2 {
+                        record_asn.parse().unwrap_or(0)
+                    } else {
+                        0
+                    },
+                    as_name: record_as_name.to_owned(),
+                    as_domain: record_as_domain.to_owned(),
+                }
+            } else {
+                ASInfo {
+                    asn: 0,
+                    as_name: String::new(),
+                    as_domain: String::new(),
+                }
+            };
+
+            let (asn, _) = asns.insert_full(as_info);
 
             let ip_info = pack_ip_info(
                 asn.try_into().unwrap(),
@@ -131,52 +208,43 @@ fn load_data(data_path: &str) -> IPData {
                 continent.try_into().unwrap(),
             );
 
-            table.insert(network, ip_info);
-        } else {
-            record.0.push_str("/32");
+            let (ip_index, _) = ip_data.insert_full(ip_info);
 
-            if let Ok(network) = IpNet::from_str(&record.0) {
-                let (continent, _) = continents.insert_full(ContinentInfo {
-                    continent: record.3,
-                    continent_code: record.4.into_bytes().try_into().unwrap(),
-                });
-                let (country, _) = countries.insert_full(CountryInfo {
-                    country: record.1,
-                    country_code: record.2.into_bytes().try_into().unwrap(),
-                });
-                let (asn, _) = asns.insert_full(ASInfo {
-                    asn: if record.5.len() > 2 {
-                        record.5[2..].parse().unwrap_or(0)
-                    } else {
-                        0
-                    },
-                    as_name: record.6,
-                    as_domain: record.7,
-                });
-
-                let ip_info = pack_ip_info(
-                    asn.try_into().unwrap(),
-                    country.try_into().unwrap(),
-                    continent.try_into().unwrap(),
-                );
-
-                table.insert(network, ip_info);
+            if let Ok(network) = IpNet::from_str(record_network) {
+                table.insert(network, U24::new(ip_index as u32));
             } else {
-                panic!("Invalid network {}", record.0)
+                let mut network_clone = record_network.to_string();
+                network_clone.push_str("/32");
+
+                if let Ok(network) = IpNet::from_str(&network_clone) {
+                    table.insert(network, U24::new(ip_index as u32));
+                } else {
+                    panic!("Invalid network {}", record_network)
+                }
             }
         }
     }
+
+    println!("{} unique entries", ip_data.len());
 
     IPData {
         table,
         continents,
         countries,
         asns,
+        ip_data,
     }
 }
 
-fn to_record(ip_data: &IPData, network: IpNet, ip_info: IpInfo) -> Option<pb::IpResponse> {
-    let (asn, country, continent) = unpack_ip_info(ip_info);
+fn to_record(ip_data: &IPData, network: IpNet, ip_info: &U24) -> Option<pb::IpResponse> {
+    let ip_index: u32 = ip_info.read();
+
+    let (asn, country, continent) = unpack_ip_info(
+        *ip_data
+            .ip_data
+            .get_index(ip_index.try_into().unwrap())
+            .unwrap(),
+    );
 
     let country_info = ip_data.countries.get_index(country as usize)?;
     let as_info = ip_data.asns.get_index(asn as usize)?;
@@ -208,7 +276,7 @@ impl pb::lookup_server::Lookup for LookupServer {
         let response = if let Ok(parsed_ip) = IpAddr::from_str(&ip) {
             let network = IpNet::from(parsed_ip);
             if let Some(found) = self.ip_data.table.longest_match(&network) {
-                to_record(&self.ip_data, found.0, *found.1)
+                to_record(&self.ip_data, found.0, found.1)
             } else {
                 None
             }
@@ -235,7 +303,7 @@ impl pb::lookup_server::Lookup for LookupServer {
                         let response = if let Ok(parsed_ip) = IpAddr::from_str(&v.ip) {
                             let network = IpNet::from(parsed_ip);
                             if let Some(found) = ip_data.table.longest_match(&network) {
-                                to_record(&ip_data, found.0, *found.1)
+                                to_record(&ip_data, found.0, found.1)
                             } else {
                                 None
                             }
@@ -263,7 +331,7 @@ impl pb::lookup_server::Lookup for LookupServer {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    assert!(args.len() == 2, "Usage: ./server <data.csv>");
+    assert!(args.len() == 2, "Usage: ./server <data.parquet>");
 
     println!("Starting...");
 
